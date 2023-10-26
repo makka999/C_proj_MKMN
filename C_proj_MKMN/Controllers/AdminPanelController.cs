7374a9174b9cd830366d0e5ac76e220d4baad98b
Michał <michallzxy@gmail.com>
2023-10-26 17:55:13 +0200

    lista

--- C_proj_MKMN/Controllers/AdminPanelController.cs
+++ C_proj_MKMN/Controllers/AdminPanelController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using C_proj_MKMN.Data;
 
 namespace C_proj_MKMN.Controllers
 {
@@ -9,6 +10,8 @@ namespace C_proj_MKMN.Controllers
     {
 
         private readonly UserManager<UserModel> _userManager;
+        private readonly ApplicationDbContext _context;
+        
 
         public AdminPanelController(UserManager<UserModel> userManager)
         {
@@ -56,6 +59,91 @@ namespace C_proj_MKMN.Controllers
         }
 
         // GET: AdminPanelController/Edit/5
+        public async Task<IActionResult> EditUsers(string id)
+        {
+            if (id == null)
+            {
+                return NotFound(); // Handle the case where the user ID is not provided
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            Console.WriteLine(user);
+            
+            if (user == null)
+            {
+                return NotFound(); // Handle the case where the user is not found
+            }
+
+            return View(user); // Pass the user object to the view
+        }
+
+        public async Task<IActionResult> PatchUser(string id, UserModel updatedUser)
+        {
+            if (id != updatedUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (string.IsNullOrEmpty(id) || updatedUser == null)
+            {
+                return NotFound();
+            }
+
+            // Retrieve the existing user from the database
+            var existingUser = await _userManager.FindByIdAsync(id);
+
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            existingUser.Id = updatedUser.Id; 
+          
+            //existingUser.PasswordLengthRequirement = updatedUser.PasswordLengthRequirement;
+            //existingUser.Version = Guid.NewGuid().ToByteArray();
+            existingUser.Email = updatedUser.Email;
+            existingUser.NormalizedEmail = updatedUser.Email.ToUpper();
+            existingUser.UserName = updatedUser.UserName;
+            existingUser.NormalizedUserName = updatedUser.UserName.ToUpper();
+            //existingUser.LockoutEnabled = updatedUser.LockoutEnabled;
+            //existingUser.LockoutEnd = updatedUser.LockoutEnd;
+            existingUser.EmailConfirmed = updatedUser.EmailConfirmed;
+
+            var result = await _userManager.UpdateAsync(existingUser);
+
+            if (result.Succeeded)
+            {
+                // Handle successful update
+                return RedirectToAction("GetUsers");
+            }
+            else
+            {
+                // Handle update failure, check result.Errors for error details
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("EditUsers", updatedUser); // Return to edit view with error messages
+            }
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditUsers(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
         public ActionResult Edit(int id)
         {
             return View();
--- C_proj_MKMN/Data/Migrations/20231026145516_initM.Designer.cs
+++ C_proj_MKMN/Data/Migrations/20231026145516_initM.Designer.cs
@@ -4,6 +4,7 @@ using C_proj_MKMN.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace C_proj_MKMN.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231026145516_initM")]
+    partial class initM
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
